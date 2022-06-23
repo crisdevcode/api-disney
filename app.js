b44acfc3b18b70b0cb398bd306491537ff0e9b96
Cristian Unigarro <ungcrisfit@gmail.com>
2022-06-23 13:40:16 -0500

    feat: add security settings

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+
 import globalErrorHandler from './controllers/errorController.js';
 import AppError from './utils/appError.js';
 import characterRouter from './routes/characterRoutes.js';
@@ -7,12 +12,33 @@ import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
-// 1) MIDDLLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
 app.use(express.json());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
