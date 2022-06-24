04c86d4ee79b9d2a6a63ad079b1f0ec25fd141d8
Cristian Unigarro <ungcrisfit@gmail.com>
2022-06-24 07:52:18 -0500

    chore: delete unnecessary lines

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,9 @@
 import express from 'express';
-import { methods as userController } from '../controllers/userController.js';
 import { methods as authController } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
-
-router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
-router
-  .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
 
 export default router;
